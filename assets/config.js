5aa9e90a150b52634162b934815811e0a3b0ad13
vidyaprajapati <60651134+vidyaprajapati@users.noreply.github.com>
2026-08-24 20:35:41 +0530

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -2,4 +2,4 @@
 // After deploying the Apps Script web app (see apps-script/Code.gs and SETUP.md),
 // paste the deployment URL below. It looks like:
 // https://script.google.com/macros/s/AKfycb.../exec
-window.VISUTRA_API_URL = "https://script.google.com/macros/s/AKfycbyiXjV1wMkVLQCiFYC6AymcpAZqoX0OgPYXzQT-MbG7CRwVRAihlYQqTTyr_HHnJIqH/exec";
+window.VISUTRA_API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
